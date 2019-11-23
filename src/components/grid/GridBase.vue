51808ee3466d18bcaf5d22064ab789be1236487b
alex-greff <alex.j.greff@gmail.com>
2019-11-23 18:46:01 -0500

    Completed GridBase component. Updated GrideBase storyboard to clearly demonstrate usage of the component

--- src/components/grid/GridBase.vue
+++ src/components/grid/GridBase.vue
@@ -1,18 +1,53 @@
 <template>
     <div 
-        class="GridBase"
+        :class="[
+            'GridBase',
+            lockHorizontalLegend ? 'lock-horizontal-legend' : null,
+        ]"
     >
-        <div 
-            class="GridBase__grid"
-            :style="gridStyles"
+        <div
+            v-if="enableHorizontalLegend" 
+            :class="['GridBase__horizontal-legend', horizontalLegendClass]"
+            :style="[horizontalLegendComputedStyles, horizontalLegendStyle]"
+            :id="[horizontalLegendId]"
         >
+            <slot name="horizontal-legend"></slot>
+        </div>
 
+        <div 
+            class="GridBase__container"
+            ref="containerEl"
+        >
+            <div 
+                :class="['GridBase__vertical-legend', verticalLegendClass]"
+                :style="[verticalLegendComputedStyles, verticalLegendStyle]"
+                :id="[verticalLegendId]"
+                ref="vertLegendEl"
+            >
+                <slot name="vertical-legend"></slot>
+            </div>
+
+            <div class="GridBase__grid-container">
+                <div 
+                    :class="['GridBase__grid', gridClass]"
+                    :style="[gridComputedStyles, gridStyle]"
+                    :id="[gridId]"
+                    ref="gridEl"
+                >
+                    <!-- Default slot -->
+                    <slot></slot>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
-import { PositiveNumber, CSSGridLength } from "../../validators";
+import { PositiveNumber, CSSGridLength, CSSLength } from "../../validators";
+import * as Utilities from "../../utilities";
+
+const SCROLLBAR_WIDTH = "5px";
+const DEBOUNC_RATE = 50;
 
 export default {
     props: {
@@ -36,6 +71,7 @@ export default {
             default: "15rem",
             validator: CSSGridLength
         },
+        // Legend props
         enableHorizontalLegend: {
             type: Boolean,
             default: false
@@ -43,22 +79,165 @@ export default {
         enableVerticalLegend: {
             type: Boolean,
             default: false
+        },
+        horizontalLegendHeight: {
+            type: String,
+            default: "auto",
+            validator: CSSLength
+        },
+        verticalLegendWidth: {
+            type: String,
+            default: "auto",
+            validator: CSSLength
+        },
+        lockHorizontalLegend: {
+            type: Boolean,
+            default: false
+        },
+        // Internal class props
+        gridClass: {
+            type: String,
+            default: null
+        },
+        gridId: {
+            type: String,
+            default: null
+        },
+        gridStyle: {
+            type: Object,
+            default: null
+        },
+        horizontalLegendClass: {
+            type: String,
+            default: null
+        },
+        horizontalLegendId: {
+            type: String,
+            default: null
+        },
+        horizontalLegendStyle: {
+            type: Object,
+            default: null
+        },
+        verticalLegendClass: {
+            type: String,
+            default: null
+        },
+        verticalLegendId: {
+            type: String,
+            default: null
+        },
+        verticalLegendStyle: {
+            type: Object,
+            default: null
         }
-        // horizontalData: {
-        //     type: Array, // Array of strings
-        //     default: () => []
-        // }
+    },
+    data() {
+        return {
+            onResizeDebounced: Utilities.debounce(this.onResize, DEBOUNC_RATE),
+            horizLengendRightPad: "0",
+            horizLegendLeftPad: "0",
+        }
     },
     computed: {
-        gridStyles() {
+        gridComputedStyles() {
             return {
-                "grid-template-columns": `repeat(${this.cellsWide}, ${this.cellWidth})`,
-                "grid-template-rows": `repeat(${this.cellsHigh}, ${this.cellHeight})`
+                "grid-template-columns": this.templateColumns(),
+                "grid-template-rows": this.templateRows()
+            }
+        },
+        horizontalLegendComputedStyles() {
+            return {
+                "height": this.horizontalLegendHeight,
+                // "padding-left": (this.enableVerticalLegend) ? this.verticalLegendWidth : "0",
+                "grid-template-columns": this.templateColumns("horiz-legend"),
+                "grid-template-rows": "1fr",
+                "padding-right": this.horizLengendRightPad,
+                "padding-left": this.horizLegendLeftPad,
+            }
+        },
+        verticalLegendComputedStyles() {
+            return {
+                "width": this.verticalLegendWidth,
+                "grid-template-columns": "1fr",
+                "grid-template-rows": this.templateRows("vert-legend")
+            }
+        },
+        templateColumns() {
+            return (cellPrefix = "cell") => {
+                let templateStr = "[";
+
+                for (let cellNum = 1; cellNum <= this.cellsWide; cellNum++) {
+                    const cellName = `${cellPrefix}-${cellNum}`;
+                    templateStr += `${cellName}-start] ${this.cellWidth} [${cellName}-end `;
+                }
+
+                templateStr += "]";
+
+                return templateStr;
+            }
+        },
+        templateRows() {
+            return (cellPrefix = "cell") => {
+                let templateStr = "[";
+
+                for (let cellNum = 1; cellNum <= this.cellsHigh; cellNum++) {
+                    const cellName = `${cellPrefix}-${cellNum}`;
+                    templateStr += `${cellName}-start] ${this.cellHeight} [${cellName}-end `;
+                }
+
+                templateStr += "]";
+
+                return templateStr;
             }
         }
     },
+    methods: {
+        computeHorizLegendPaddings() {
+            if (!this.enableHorizontalLegend) return;
+
+            // Compute padding-left amount
+
+            if (this.enableVerticalLegend) {
+                const vertLegendEl = this.$refs.vertLegendEl;
+                this.horizLegendLeftPad = `${vertLegendEl.offsetWidth}px`;
+            } else {
+                this.horizLegendLeftPad = "0";
+            }
+
+            if (!this.lockHorizontalLegend) return;
+
+            // Compute padding right ammount
+        
+            const containerEl = this.$refs.containerEl;
+            const gridEl = this.$refs.gridEl;
+
+            // Grid is scrolling
+            const gridScrolling = gridEl.scrollHeight > containerEl.offsetHeight;
+
+            // Add padding to the horizontal legend container to account for the scrollbar
+            // (this makes sure the grids line up properly)
+            this.horizLengendRightPad = (gridScrolling) ? SCROLLBAR_WIDTH : "0";
+        },
+        onResize() {
+            this.computeHorizLegendPaddings();
+        },
+        // NOTE: intermediate functions, do not use
+        __onResize__() {
+            this.onResizeDebounced();
+        }
+    },
     mounted() {
-        console.log(this.$store);
+        // Setup window resize listener
+        this.$nextTick(() => {
+            window.addEventListener('resize', this.__onResize__);
+            // Fire resize event on load (for initialization purposes)
+            this.__onResize__();
+        });
+    },
+    beforeDestroy() {
+        // Remove window resize listener
+        window.removeEventListener('resize', this.__onResize__);
     }
 }
 </script>
@@ -72,10 +251,54 @@ export default {
 
         overflow: auto;
 
-        background-color: rgb(68, 68, 68); // TODO: theme 
+        display: flex;
+        flex-direction: column;
 
-        & .GridBase__grid {
+        & .GridBase__horizontal-legend {
+            flex-grow: 0;
+            flex-shrink: 0;
+
             display: grid;
+        }
+
+        & .GridBase__container {
+            flex-grow: 1;
+            flex-shrink: 1;
+
+            display: flex;
+            flex-direction: row;
+            align-items: stretch;
+
+            & .GridBase__vertical-legend {
+                flex-grow: 0;
+                flex-shrink: 0;
+
+                display: grid;
+            }
+
+            & .GridBase__grid-container {
+                flex-grow: 1;
+                flex-shrink: 1;
+
+                & .GridBase__grid {
+                    display: grid;
+
+                    border-radius: 5rem;
+                }
+
+            }   
+        }
+
+        &.lock-horizontal-legend {
+            overflow: hidden;
+
+            & .GridBase__container {
+                overflow: auto;
+            }
+        }
+
+        &:not(.lock-horizontal-legend) {
+            overflow: auto;
         }
     }
 </style>
--- stories/02-Grid.stories.js
+++ stories/02-Grid.stories.js
@@ -8,16 +8,92 @@ export default {
 };
 
 export const main = () => ({
+    template: `
+        <grid-base
+            :cells-wide="cellsWide"
+            :cells-high="cellsHigh"
+            cell-width="1fr"
+            cell-height="15rem"
+            :enable-horizontal-legend="true"
+            :enable-vertical-legend="true"
+            vertical-legend-width="auto"
+            horizontal-legend-height="auto"
+            :lock-horizontal-legend="true"
+
+            :style="{
+                'background-color': 'rgb(68, 68, 68)'
+            }"
+        >
+            <template
+                #horizontal-legend
+            >
+                <!-- Mock horizontal legend cells -->
+                <div
+                    v-for="n in cellsWide"
+                    :key="n"
+                    :style="{
+                        'border': '1px solid black',
+                        'grid-column': 'horiz-legend-' + n + '-start / horiz-legend-' + n + '-end',
+                        'grid-row': '1'
+                    }"
+                >
+                    Horiz {{ n }}
+                </div>
+            </template>
+
+            <template
+                #vertical-legend
+            >
+                <!-- Mock vertical legend cells -->
+                <div
+                    v-for="n in cellsHigh"
+                    :key="n"
+                    :style="{
+                        'width': '100%',
+                        'height': '100%',
+                        'border': '1px solid black',
+                        'grid-column': '1',
+                        'grid-row': 'vert-legend-' + n + '-start / vert-legend-' + n + '-end'
+                    }"
+                >
+                    Vert {{ n }}
+                </div>
+            </template>
+
+            <template>
+                <!-- Mock grid cells -->
+                <div
+                    v-for="n in cellsHigh * cellsWide"
+                    :key="n"
+                    :style="{
+                        'width': '100%',
+                        'height': '100%',
+                        'border': '1px solid black',
+                        'grid-column': 'cell-' + (getCol(n-1) + 1) + '-start / cell-' + (getCol(n-1) + 1) + '-end',
+                        'grid-row': 'cell-' + (getRow(n-1) + 1) + '-start / cell-' + (getRow(n-1) + 1) + '-end'
+                    }"
+                >
+                    Cell {{ n }}
+                </div>
+            </template>
+        </grid-base>
+    `,
     components: {
         GridBase,
         GridItem
     },
-    template: `
-        <grid-base
-            :cells-wide="5"
-            :cells-high="5"
-        >
-
-        </grid-base>
-    `
+    data() {
+        return {
+            cellsWide: 5,
+            cellsHigh: 5
+        }
+    },
+    methods: {
+        getRow(n) { // Note: n is zero-based
+            return Math.floor(n / this.cellsWide);
+        },
+        getCol(n) {
+            return n % this.cellsWide;
+        },
+    }
 });
