7d0fb826d798a3e2852f80b8f1017d7914ce1d6b
alex-greff <alex.j.greff@gmail.com>
2019-11-24 17:51:12 -0500

    Added a validator the CSS cell functions (minmax and fit-content)

--- src/validators/index.js
+++ src/validators/index.js
@@ -8,6 +8,17 @@ export const Direction = val => Utilities.isDirection(val);
 export const CSSGridLength = val => Utilities.isCSSLength(val) 
                                     || Utilities.isCSSFrUnit(val);
 
+// minmax or fit-content functions
+export const CSSGridFunc = val => {
+    const rMinMax = /^minmax\(([\d|\w|%|-]+),\s?([\d|\w|%|-]+)\)$/gm
+    const rFitContent = /^fit-content\(([\d|\w|%|-]+)\)/gm;
+
+    const minMaxMatch = rMinMax.exec(val);
+    const fitContentMatch = rFitContent.exec(val);
+
+    return (minMaxMatch || fitContentMatch);
+}
+
 export const PositiveNumber = val => val > 0;
 export const ZeroNumber = val => val === 0;
 export const NegativeNumber = val => val < 0;
